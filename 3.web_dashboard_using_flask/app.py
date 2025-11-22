5deaf63559e1f89ff32e2d6084df883c7b660461
Darshan Kharbikar <darshankharbikar265@gmail.com>
2025-11-22 17:54:25 +0530

    Update app.py

--- 3.web_dashboard_using_flask/app.py
+++ 3.web_dashboard_using_flask/app.py
@@ -15,7 +15,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-users = {'pi': generate_password_hash('1994')}
+users = {'pi': generate_password_hash('1234')}
 
 class User(UserMixin):
     def __init__(self, username):
